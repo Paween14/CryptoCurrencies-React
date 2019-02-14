0ab9f806331c8e1df8a1d1c79bb860deeee07596
unknown <paweennuch.boonamnuay@gmail.com>
2019-02-14 16:50:12 +0200

    Updates

---
+++ precache-manifest.009e9bf1a676f373f89443a0b831892b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "b458578cb4918359c291f21ade2806a8",
+    "url": "/CryptoCurrencies-React/static/media/cryptocoins.b458578c.woff2"
+  },
+  {
+    "revision": "284a85febc6652ce35b2ca344c2aae49",
+    "url": "/CryptoCurrencies-React/static/media/cryptocoins.284a85fe.ttf"
+  },
+  {
+    "revision": "194ac4725d0152963c9b383933e38a49",
+    "url": "/CryptoCurrencies-React/static/media/cryptocoins.194ac472.woff"
+  },
+  {
+    "revision": "1138dc26a0ca99aa5f5a",
+    "url": "/CryptoCurrencies-React/static/js/runtime~main.1138dc26.js"
+  },
+  {
+    "revision": "73802292717427ba67ca",
+    "url": "/CryptoCurrencies-React/static/js/main.73802292.chunk.js"
+  },
+  {
+    "revision": "601dfec4956daf1e07f0",
+    "url": "/CryptoCurrencies-React/static/js/1.601dfec4.chunk.js"
+  },
+  {
+    "revision": "73802292717427ba67ca",
+    "url": "/CryptoCurrencies-React/static/css/main.48e7a08f.chunk.css"
+  },
+  {
+    "revision": "601dfec4956daf1e07f0",
+    "url": "/CryptoCurrencies-React/static/css/1.cbae9ebb.chunk.css"
+  },
+  {
+    "revision": "b98013e934561a81305e5305590812c8",
+    "url": "/CryptoCurrencies-React/index.html"
+  }
+];
